1869c39623fecca43f0789f0e2770e7644a6e390
NaveenGnanappanavar <30663471+NaveenGnanappanavar@users.noreply.github.com>
2017-09-01 15:59:59 +0530

    Question 1

---
+++ demo-6/src/main/java/com/example/demo/Demo6Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class Demo6Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Demo6Application.class, args);
+		
+	
+	}
+}
